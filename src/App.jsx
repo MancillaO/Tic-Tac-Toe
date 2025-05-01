2fb5114ffd0283f95899e4dd53d6e7749492ccf6
MancillaO <edgarmncllav04@gmail.com>
2025-05-01 16:35:02 -0600

    Guardar partida en localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import { WinnerModal } from './components/WinnerModal.jsx'
 import { Board } from './components/Board.jsx'
 import { Turns } from './components/Turns.jsx'
 import { checkWinner, checkEndGame } from './logic/board.js'
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
 import { TURNS } from './constants.js'
 import confetti from 'canvas-confetti'
 import './App.css'
 
 export default function App () {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (!boardFromStorage) return Array(9).fill(null)
+    return JSON.parse(boardFromStorage)
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
   const [winner, setWinner] = useState(null)
 
   const updateBoard = (index) => {
@@ -25,13 +35,16 @@ export default function App () {
     } else if (checkEndGame(newBoard)) {
       setWinner(false)
     }
-    setTurn(turn === TURNS.X ? TURNS.O : TURNS.X)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    saveGameToStorage(newBoard, newTurn)
   }
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    resetGameStorage()
   }
 
   return (
